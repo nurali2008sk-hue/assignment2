01ee65056f2b4a3f2b8431376057cac718962277
nurali <nurali2008sk@gmail.com>
2025-12-29 15:21:34 +0500

    i have made a code for truck.java

---
+++ src/Truck.java
@@ -0,0 +1,24 @@
+public class Truck extends Vehicle {
+    private double capacity;
+    private int numAxles;
+
+    public Truck(String brand, int year, double capacity, int numAxles) {
+        super(brand, year);
+        this.capacity = capacity;
+        this.numAxles = numAxles;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println(brand + " truck diesel engine starts with a heavy rumble.");
+    }
+
+    @Override
+    public void stopEngine() {
+        System.out.println(brand + " truck engine shuts down with a hiss.");
+    }
+
+    public double calculateEfficiency() {
+        return capacity / numAxles;
+    }
+}
